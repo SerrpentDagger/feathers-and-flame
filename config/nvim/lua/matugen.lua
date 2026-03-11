5aa969d8f529f18eb0d084ca8c405c2b16f7c408
SerrpentDagger <falcon-lark-mummy@duck.com>
2026-03-11 22:04:56 +0100

    keybinds.kdl reflects cmd dir, win rule tweaks

--- config/nvim/lua/matugen.lua
+++ config/nvim/lua/matugen.lua
@@ -3,23 +3,23 @@ local M = {}
 function M.setup()
   require("base16-colorscheme").setup({
     -- Background tones
-    base00 = "#033a23", -- Default Background
-    base01 = "#0f5738", -- Lighter Background (status bars)
-    base02 = "#0b5033", -- Selection Background
-    base03 = "#698076", -- Comments, Invisibles
+    base00 = "#17271c", -- Default Background
+    base01 = "#26402e", -- Lighter Background (status bars)
+    base02 = "#223a29", -- Selection Background
+    base03 = "#5d7264", -- Comments, Invisibles
     -- Foreground tones
-    base04 = "#afb6b3", -- Dark Foreground (status bars)
+    base04 = "#afb6b1", -- Dark Foreground (status bars)
     base05 = "#f2f3f2", -- Default Foreground
     base06 = "#f2f3f2", -- Light Foreground
     base07 = "#f2f3f2", -- Lightest Foreground
     -- Accent colors
     base08 = "#fd4663", -- Variables, XML Tags, Errors
-    base09 = "#3d8bf5", -- Integers, Constants
-    base0A = "#3de7f5", -- Classes, Search Background
-    base0B = "#55f6b2", -- Strings, Diff Inserted
-    base0C = "#86b6f9", -- Regex, Escape Chars
-    base0D = "#86f9c8", -- Functions, Methods
-    base0E = "#86f0f9", -- Keywords, Storage
+    base09 = "#66accc", -- Integers, Constants
+    base0A = "#5cd6c1", -- Classes, Search Background
+    base0B = "#67e48e", -- Strings, Diff Inserted
+    base0C = "#96cfe9", -- Regex, Escape Chars
+    base0D = "#93ecaf", -- Functions, Methods
+    base0E = "#96e9da", -- Keywords, Storage
     base0F = "#900017", -- Deprecated, Embedded Tags
   })
 end
