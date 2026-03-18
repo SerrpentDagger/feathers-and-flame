1fe6ad01dfdc5f995207c2a8116c362332872e91
SerrpentDagger <falcon-lark-mummy@duck.com>
2026-03-18 23:32:54 +0100

    website rules, notif widget color fix

--- config/nvim/lua/matugen.lua
+++ config/nvim/lua/matugen.lua
@@ -3,23 +3,23 @@ local M = {}
 function M.setup()
   require("base16-colorscheme").setup({
     -- Background tones
-    base00 = "#291a14", -- Default Background
-    base01 = "#452b21", -- Lighter Background (status bars)
-    base02 = "#3e271e", -- Selection Background
-    base03 = "#74655f", -- Comments, Invisibles
+    base00 = "#252e0f", -- Default Background
+    base01 = "#3e4c1a", -- Lighter Background (status bars)
+    base02 = "#384517", -- Selection Background
+    base03 = "#707661", -- Comments, Invisibles
     -- Foreground tones
-    base04 = "#b6b1af", -- Dark Foreground (status bars)
-    base05 = "#f3f2f2", -- Default Foreground
-    base06 = "#f3f2f2", -- Light Foreground
-    base07 = "#f3f2f2", -- Lightest Foreground
+    base04 = "#b4b6af", -- Dark Foreground (status bars)
+    base05 = "#f3f3f2", -- Default Foreground
+    base06 = "#f3f3f2", -- Light Foreground
+    base07 = "#f3f3f2", -- Lightest Foreground
     -- Accent colors
     base08 = "#fd4663", -- Variables, XML Tags, Errors
-    base09 = "#b3d35f", -- Integers, Constants
-    base0A = "#d6bd5c", -- Classes, Search Background
-    base0B = "#e48a67", -- Strings, Diff Inserted
-    base0C = "#d2e996", -- Regex, Escape Chars
-    base0D = "#ecac93", -- Functions, Methods
-    base0E = "#e9d896", -- Keywords, Storage
+    base09 = "#51e179", -- Integers, Constants
+    base0A = "#71e151", -- Classes, Search Background
+    base0B = "#c2e567", -- Strings, Diff Inserted
+    base0C = "#92ecab", -- Regex, Escape Chars
+    base0D = "#d3ec92", -- Functions, Methods
+    base0E = "#a6ec92", -- Keywords, Storage
     base0F = "#900017", -- Deprecated, Embedded Tags
   })
 end
