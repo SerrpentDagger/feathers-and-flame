b0a7cc676b6ed0c294579bbd05b78e27d797f80b
SerrpentDagger <falcon-lark-mummy@duck.com>
2026-03-18 01:20:31 +0100

    vlc media info, hide notif tray if none

--- config/nvim/lua/matugen.lua
+++ config/nvim/lua/matugen.lua
@@ -3,23 +3,23 @@ local M = {}
 function M.setup()
   require("base16-colorscheme").setup({
     -- Background tones
-    base00 = "#181c26", -- Default Background
-    base01 = "#272f3f", -- Lighter Background (status bars)
-    base02 = "#232a39", -- Selection Background
-    base03 = "#616775", -- Comments, Invisibles
+    base00 = "#291a14", -- Default Background
+    base01 = "#452b21", -- Lighter Background (status bars)
+    base02 = "#3e271e", -- Selection Background
+    base03 = "#74655f", -- Comments, Invisibles
     -- Foreground tones
-    base04 = "#afb1b6", -- Dark Foreground (status bars)
-    base05 = "#f2f2f3", -- Default Foreground
-    base06 = "#f2f2f3", -- Light Foreground
-    base07 = "#f2f2f3", -- Lightest Foreground
+    base04 = "#b6b1af", -- Dark Foreground (status bars)
+    base05 = "#f3f2f2", -- Default Foreground
+    base06 = "#f3f2f2", -- Light Foreground
+    base07 = "#f3f2f2", -- Lightest Foreground
     -- Accent colors
     base08 = "#fd4663", -- Variables, XML Tags, Errors
-    base09 = "#aa66cc", -- Integers, Constants
-    base0A = "#6e5cd6", -- Classes, Search Background
-    base0B = "#6791e4", -- Strings, Diff Inserted
-    base0C = "#cd96e9", -- Regex, Escape Chars
-    base0D = "#93b1ec", -- Functions, Methods
-    base0E = "#a296e9", -- Keywords, Storage
+    base09 = "#b3d35f", -- Integers, Constants
+    base0A = "#d6bd5c", -- Classes, Search Background
+    base0B = "#e48a67", -- Strings, Diff Inserted
+    base0C = "#d2e996", -- Regex, Escape Chars
+    base0D = "#ecac93", -- Functions, Methods
+    base0E = "#e9d896", -- Keywords, Storage
     base0F = "#900017", -- Deprecated, Embedded Tags
   })
 end
