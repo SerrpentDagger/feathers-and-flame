77bf62389c829db0b087544eb3bb33dde3c56536
SerrpentDagger <falcon-lark-mummy@duck.com>
2026-03-18 01:15:54 +0100

    pkg setup scripts/structure

--- script/packages.sh
+++ script/packages.sh
@@ -9,3 +9,10 @@ if ! [[ -e "$SCALEP/user-selected.packages" ]]; then
 fi
 source "$SCALES/from-file.sh" user-selected.packages
 source "$SCALES/from-file-aur.sh" user-selected-aur.packages
+
+function pkg_installed() {
+	pacman -Q "$1" 2>/dev/null
+}
+# Run setup scripts based on what was installed
+pkg_installed virt-manager && source "$SCALEPS/virt-manager.sh"
+pkg_installed localsend && source "$SCALEPS/localsend.sh"
---
+++ script/pkg-setups/virt-manager.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Setting up additional configurations for Qemu VMs in virt-manager"
+# From Cachy Os doc page:
+# This will install the needed packages (note the "Windows 11" note below):
+sudo pacman -S qemu-full
+# Force libvirt to use iptables
+echo 'firewall_backend = "iptables"' | sudo tee -a /etc/libvirt/network.conf
+# This will add the user to the "libvirt" group so they can use it:
+sudo usermod -aG libvirt "$USER"
+# LXC backend (optional, for linux containers, enabling both backends does not conflict):
+systemctl enable --now libvirtd.service
+# QEMU backend (for VMs):
+systemctl enable --now libvirtd.socket
+# This will bring Internet up in a VM whenever one starts:
+sudo virsh net-autostart default
