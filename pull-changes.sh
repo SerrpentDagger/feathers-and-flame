5b144bd23ac2028b2cffba957b8491dfc08239f0
SerrpentDagger <falcon-lark-mummy@duck.com>
2026-04-01 21:54:03 +0200

    Alacritty in pull-changes

--- pull-changes.sh
+++ pull-changes.sh
@@ -10,7 +10,9 @@ cd "$SCALEC" || exit
 mv niri "$scale_backup/niri"
 mv noctalia "$scale_backup/noctalia"
 mv nvim "$scale_backup/nvim"
+mv alacritty "$scale_backup/alacritty"
 
 cp -R ~/.config/niri niri
 cp -R ~/.config/noctalia noctalia
 cp -R ~/.config/nvim nvim
+cp -R ~/.config/alacritty alacritty
