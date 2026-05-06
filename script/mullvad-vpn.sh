28fbb7139f99e8913cc5ddc81adb86bd5aa2fc7a
SerrpentDagger <falcon-lark-mummy@duck.com>
2026-05-06 14:49:03 +0200

    Make resilient to pacman failure.

--- script/mullvad-vpn.sh
+++ script/mullvad-vpn.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo pacman -Syu --noconfirm --needed mullvad-vpn
+if sudo pacman -Syu --noconfirm --needed mullvad-vpn; then
+	sudo systemctl enable --now mullvad-daemon
 
-sudo systemctl enable --now mullvad-daemon
-
-if gum confirm "Block network until VPN is up?"; then
-	sudo systemctl enable mullvad-early-boot-blocking
+	if gum confirm "Block network until VPN is up?"; then
+		sudo systemctl enable mullvad-early-boot-blocking
+	fi
 fi
