6be8d73055617efa035d672fc608f21b4a842c1b
SerrpentDagger <falcon-lark-mummy@duck.com>
2026-05-08 22:12:56 +0200

    Enable migrations in update, diff-cp-req utility for asking permission.

--- update.sh
+++ update.sh
@@ -14,19 +14,7 @@ fi
 cd "$FEATHER_PATH"
 git pull
 
-# Check for config changes, query for update if so
-if source "$FEATHERH/sel-comps.sh" check "Configs"; then
-	source "$FEATHERH/tmp-spawn.sh"
-	source "$FEATHER_PATH/pull-changes.sh" --for-diff
-	source "$FEATHERS/configs.sh" --for-diff
-	if ! diff -r --color --label="CURRENT" --label="NEW" "$FEATHERT/config" "$FEATHERT/feather" &&
-		gum confirm "Update configs in-place? (Backups created in $FEATHER_BACKUPS) Otherwise you can just copy them from the ./config directory here."; then
-		bash "$FEATHERS/configs.sh"
-		source "$FEATHERCMD/noctalia-restart.sh"
-	fi
-	source "$FEATHERH/tmp-clear.sh"
-fi
-
+source "$FEATHERS/migrations.sh"
 source "$FEATHER_PATH/refresh.sh"
 
 source "$FEATHERH/show-done.sh"
