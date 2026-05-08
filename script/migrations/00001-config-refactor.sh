7a782e5505fec9e78cfa6bfc14e3fc4a3ca10c99
SerrpentDagger <falcon-lark-mummy@duck.com>
2026-05-09 00:59:25 +0200

    Fix must sed user

--- script/migrations/00001-config-refactor.sh
+++ script/migrations/00001-config-refactor.sh
@@ -2,4 +2,8 @@
 
 source "$HOME/.local/share/feathers-and-flame/vars.sh"
 
-source "$FEATHERH/diff-cp-req.sh" "$FEATHERC/niri" ~/.config/niri
+source "$FEATHERH/diff-cp-req.sh" "$FEATHERC/niri" "$HOME/.config/niri"
+
+source "$FEATHERH/sed-user.sh" "$HOME/.config/niri/cfg/misc.kdl"
+source "$FEATHERH/sed-user.sh" "$HOME/.config/niri/cfg/keybinds.kdl"
+source "$FEATHERH/sed-user.sh" "$HOME/.config/niri/cfg/hot-rules.kdl"
