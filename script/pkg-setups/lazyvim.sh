e47767af76cc3285508987312cd1753c3e0877d6
SerrpentDagger <falcon-lark-mummy@duck.com>
2026-06-12 01:57:46 +0200

    Backup nvim.desktop before sed, refresh nvim.desktop after updates to avoid nautilus fail.

--- script/pkg-setups/lazyvim.sh
+++ script/pkg-setups/lazyvim.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # Lazyvim for nautilus
+source "$FEATHERH/backup.sh" /usr/share/applications/nvim.desktop
 sudo sed -i "s/Exec=nvim %F/Exec=alacritty -e nvim %F/g" /usr/share/applications/nvim.desktop || true
 sudo sed -i "s/Terminal=true/Terminal=false/g" /usr/share/applications/nvim.desktop || true
