cbc5eb1e890b0f847f9286ea8a32c84503791c4d
Name <not-real@fake.com>
2026-06-12 01:32:43 +0200

    Reword prompt

--- script/migrations/00003-missing-jq-dep.sh
+++ script/migrations/00003-missing-jq-dep.sh
@@ -3,7 +3,7 @@
 source "$HOME/.local/share/feathers-and-flame/vars.sh"
 
 if ! pacman -Qi jq; then
-	gum style --bold "jq (JSON command utility) is missing, and is used in certain script functionality."
+	gum style --bold "The jq package (JSON command utility) is missing, and is used in certain script functionality."
 	if gum confirm "Install now?"; then
 		sudo pacman -S jq
 	fi
