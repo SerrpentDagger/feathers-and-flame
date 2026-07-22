170660e2b48d9bb309bddbc7ec6538b8e2d3787d
SerrpentDagger <falcon-lark-mummy@duck.com>
2026-07-22 16:31:05 +0200

    New config helper on Mod+G, Gimp moved to Mod+Shift+Ctrl+G, user-templates comment, notrap option for tmp-spawn.

--- script/helpers/tmp-spawn.sh
+++ script/helpers/tmp-spawn.sh
@@ -2,9 +2,17 @@
 
 source "$HOME/.local/share/feathers-and-flame/vars.sh"
 
+notrap="0"
+if [[ "--no-trap" == "$1" ]]; then
+	notrap=1
+	shift
+fi
+
 if [[ -z "$FEATHERT" ]]; then
 	echo "FEATHERT is not set!"
 	exit 1
 fi
 mkdir "$FEATHERT"
-trap 'source "$FEATHERH/tmp-clear.sh"' EXIT
+if [[ "$notrap" != "1" ]]; then
+	trap 'source "$FEATHERH/tmp-clear.sh"' EXIT
+fi
