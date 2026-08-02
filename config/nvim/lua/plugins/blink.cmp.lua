1963222867ea4a3d505d9b3696645e9421eef9de
SerrpentDagger <falcon-lark-mummy@duck.com>
2026-08-03 00:57:30 +0200

    Switch to super-tab preset to avoid clobbering enter key.

--- config/nvim/lua/plugins/blink.cmp.lua
+++ config/nvim/lua/plugins/blink.cmp.lua
@@ -2,9 +2,7 @@ return {
   "saghen/blink.cmp",
   opts = {
     keymap = {
-      preset = "enter",
-      ["<CR>"] = { "fallback" },
-			["<Tab>"] = { "select_and_accept" },
+      preset = "super-tab",
     },
   },
 }
