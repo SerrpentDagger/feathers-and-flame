28471202eb13530eadb9cb07004e135dac8f0a16
SerrpentDagger <falcon-lark-mummy@duck.com>
2026-08-29 16:37:12 +0200

    Wait for keypresses manually after gum changes.

--- script/helpers/show-done.sh
+++ script/helpers/show-done.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-# Display a "Done!" message with a spinner and wait for user to press any key.
-# Used by install scripts to indicate completion.
+dflt_msg='Done! Press any key to close...'
+disp_msg="${1:-"$dflt_msg"}"
 
-echo
-gum spin --title "Done! Press any key to close..." -- bash -c 'read -n 1 -s'
+# Source - https://stackoverflow.com/a/79920665
+# Posted by Caden Finkelstein, modified by community. See post 'Timeline' for change history
+# Retrieved 2026-08-29, License - CC BY-SA 4.0
+
+printf '\n%s' "$disp_msg" >"/dev/stdout"
+_t="$(stty -g)"
+stty -echo -icanon min 1 time 0
+dd bs=1 count=1 1>"/dev/null" 2>&1
+stty "${_t}"
+printf '\n' >"/dev/stdout"
