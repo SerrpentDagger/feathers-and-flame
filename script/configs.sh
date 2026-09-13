ca97006bc348ef3595afebeb9628fb6deb89e3e2
SerrpentDagger <falcon-lark-mummy@duck.com>
2026-09-13 21:24:20 +0200

    Add lockscreen deployment on install.

--- script/configs.sh
+++ script/configs.sh
@@ -51,5 +51,6 @@ if [[ "$for_diff" -eq 1 ]]; then
 	rm "$subdir/niri/noctalia.kdl"
 	rm "$subdir/nvim/lua/matugen.lua"
 else
+	source "$FEATHERCMD/lockscreen-process.sh"
 	source "$FEATHERH/sel-comps.sh" --pending remove "Configs"
 fi
