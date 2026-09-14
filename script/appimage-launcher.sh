73a1d32fe779bccfa70d96a1c05b5c7159ea876b
SerrpentDagger <falcon-lark-mummy@duck.com>
2026-09-14 11:19:06 +0200

    Fix launcher desktop user reference

--- script/appimage-launcher.sh
+++ script/appimage-launcher.sh
@@ -5,6 +5,9 @@ gum style --bold --foreground="#DDDD44" "Installing AppImage Launcher..."
 source "$HOME/.local/share/feathers-and-flame/vars.sh"
 
 source "$FEATHERH/back-cp.sh" "$FEATHERA/AppImages" "$HOME/AppImages"
-source "$FEATHERH/back-cp.sh" "$FEATHERA/appimages.desktop" "$HOME/.local/share/applications/appimages.desktop"
+
+ai_desktop="$HOME/.local/share/applications/appimages.desktop"
+source "$FEATHERH/back-cp.sh" "$FEATHERA/appimages.desktop" "$ai_desktop"
+source "$FEATHERH/sed-user.sh" "$ai_desktop"
 
 source "$FEATHERH/sel-comps.sh" --pending remove "AppImage Launcher"
